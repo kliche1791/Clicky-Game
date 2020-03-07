a6f62589af76168ac1a810e1732a74edf41b7cd9
Carlos Castrillon <carloscastrillon1991@gmail.com>
2020-03-07 12:23:47 -0500

    Updates

---
+++ precache-manifest.de88055d3a5582da59c5f8dbae2d43ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74bacb96674b833ddea72586dee40236",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "2fa683c9e6cadc868282",
+    "url": "/clicky-game/static/css/main.1cb8eb82.chunk.css"
+  },
+  {
+    "revision": "6221061988481d078117",
+    "url": "/clicky-game/static/js/2.290a4c7d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/clicky-game/static/js/2.290a4c7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fa683c9e6cadc868282",
+    "url": "/clicky-game/static/js/main.f286ba39.chunk.js"
+  },
+  {
+    "revision": "84f183f776dec52d4ec4",
+    "url": "/clicky-game/static/js/runtime-main.5c9b0e5c.js"
+  }
+]);
